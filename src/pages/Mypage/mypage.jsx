5ddb99f77d581352513254b10a9c66ba88a2758f
jaejun <ssokage090210@naver.com>
2025-08-31 15:03:05 +0900

    my페이지 연동 코드 추가
    my페이지 연동 코드 추가

--- src/pages/Mypage/mypage.jsx
+++ src/pages/Mypage/mypage.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import "../../styles/reset.css";
 import LogoutImg from "../../assets/logout.svg";
 import BookImg from "../../assets/book.svg";
@@ -19,30 +21,93 @@ import {
 } from "./mypage.styles";
 
 export default function MyPage() {
+  const { accountId } = useParams();
+  const navigate = useNavigate();
+
+  const [userData, setUserData] = useState({
+    accountId: "",
+    noticeBoardCount: 0,
+    profileImg: null,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = localStorage.getItem("accessToken");
+        if (!token) {
+          navigate("/login");
+          return;
+        }
+
+        const res = await fetch(
+          `http://43.201.199.73:8080/api/mypage/${accountId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (!res.ok) {
+          const errorText = await res.text();
+          console.log(errorText);
+          if (res.status === 401) {
+            navigate("/login");
+          }
+          return;
+        }
+
+        const data = await res.json();
+        console.log(data);
+
+        setUserData({
+          accountId: data.accountId,
+          noticeBoardCount: data.noticeBoardCount,
+          profileImg: data.profileImg || null,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+  }, [accountId, navigate]);
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    navigate("/login");
+  };
+
   return (
     <Container>
       <MypageCard>
         <LeftBox>
           <ProfileCard>
-            <ProfileImage />
+            {userData.profileImg ? (
+              <ProfileImage src={userData.profileImg} alt="프로필 이미지" />
+            ) : (
+              <ProfileImage />
+            )}
+
             <ProfileText>
-              <IdText>아이디: wjdlfdnd</IdText>
+              <IdText>아이디: {userData.accountId}</IdText>
               <PostCount>
-                작성한 게시글 수 <span>12</span>
+                작성한 게시글 수 <span>{userData.noticeBoardCount}</span>
               </PostCount>
-              <Logout>
+              <Logout onClick={handleLogout}>
                 <img src={LogoutImg} alt="로그아웃 아이콘" />
                 로그아웃
               </Logout>
             </ProfileText>
           </ProfileCard>
         </LeftBox>
+
         <RightBox>
-          <MenuItem>
+          <MenuItem as={Link} to={`/mypage/${accountId}/posts`}>
             <MenuIcon src={BookImg} alt="책 아이콘" />
             <MenuLabel>내가 작성한 게시글</MenuLabel>
           </MenuItem>
-          <MenuItem>
+          <MenuItem as={Link} to="/posts/new">
             <MenuIcon src={PenImg} alt="연필 아이콘" />
             <MenuLabel>새 게시글 작성</MenuLabel>
           </MenuItem>
