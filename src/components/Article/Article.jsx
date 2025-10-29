37c78f0d3d1253bbf804b65586de1d911dc963c0
yunho09 <jyh19310302@gmail.com>
2025-10-29 21:36:42 +0900

    욕설 필터링 추가
    욕설 필터링 추가

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,4 +1,5 @@
 import { Viewer } from "@toast-ui/react-editor";
+import { useState } from "react";
 import {
   Number,
   Title,
@@ -8,11 +9,40 @@ import {
   ArticleContentWrapper,
   GlobalViewerStyle,
 } from "./Article.styles";
+import badWords from "./badWords.json";
 
 function Article({ Numberprop, Titleprop, childrenprop, subSections }) {
   console.log(subSections);
-  const markdown = childrenprop;
-  
+  let markdown = childrenprop;
+
+  // 욕설 필터링
+  const badWord = badWords;
+
+  const censorText = (text, badWords) => {
+    let censored = text;
+
+    const SEP = "[!@#$%^&*()\\-_=+\\[\\]{};:'\",.<>?/\\\\s0-9ㄱ-ㅎㅏ-ㅣ]*";
+
+    const wordPatterns = badWords
+      .filter((w) => w && w.trim())
+      .map((word) => {
+        const escaped = word.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+        return escaped
+          .split("")
+          .map((ch) => `${ch}${SEP}`)
+          .join("");
+      });
+
+    const alternation = wordPatterns.join("|");
+    const fullPattern = `(?<![ㄱ-ㅎ가-힣a-zA-Z0-9])(?:(?:${alternation}))+?(?![ㄱ-ㅎ가-힣a-zA-Z0-9])`;
+    const regex = new RegExp(fullPattern, "gi");
+    censored = censored.replace(regex, "\\*\\*\\*");
+
+    return censored;
+  };
+
+  markdown = censorText(markdown, badWord.words);
+
   return (
     <>
       <GlobalViewerStyle />
